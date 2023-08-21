642cd9edde7e3e410ff9c3625b87f6abaeeb6325
DeeyaSingh <deeya29singh@gmail.com>
2023-08-21 20:54:41 +0530

    Updated rule detail page display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,40 @@
 function showRuleDetails(ruleName) {
-  var ruleParam = ruleName.replace(/\s/g, '-');
-  $.get("/assets/data/rules/" + ruleParam + ".yml", function(data) {
-    // Parse the YAML data using js-yaml
-    var ruleData = jsyaml.load(data);
-    console.log(ruleData);
+  var modifiedRule = ruleName ? ruleName.replace(/\s/g, '-') : '';
+  $.get("/assets/data/rules/" + modifiedRule + ".yml", function(data) 
+    {
+    const ruleStartIndex = data.indexOf('features:');
+    const featuresYaml = data.slice(ruleStartIndex);
+    const rule = jsyaml.load(data);
 
-    
-  });
+    $('#rule-name').text(rule.rule.meta.name);
+    $('#rule-details').html(`
+      <h2>Details</h2>
+      <p><strong>Namespace:</strong> ${rule.rule.meta.namespace}</p>
+      <p><strong>Authors:</strong> ${rule.rule.meta.authors.join(', ')}</p>
+      <p><strong>Scope:</strong> ${rule.rule.meta.scope ? rule.rule.meta.scope : ''}</p>
+      <p><strong>ATT&CK:</strong> ${rule.rule.meta['att&ck'] ? rule.rule.meta['att&ck'].join(', ') : ''}</p>
+      <p><strong>MBC:</strong> ${rule.rule.meta.mbc ? rule.rule.meta.mbc.join(', ') : ''}</p>
+      <p><strong>References:</strong> ${rule.rule.meta.references ? rule.rule.meta.references.join(', ') : ''}</p>
+      <p><strong>Examples:</strong> ${rule.rule.meta.examples ? rule.rule.meta.examples.join(', ') : ''}</p>
+      <pre>${featuresYaml}</pre>
+    `);
+  }, 'text');
 }
 
-// A function to handle a click on a rule link
 function onRuleLinkClick(ruleName) {
-  // Prevent the default link behavior
   event.preventDefault();
-
-  // Show the rule details
   showRuleDetails(ruleName);
 }
 
 $(document).ready(function() {
-  // Event listener for the search button
   $('#searchButton').click(function() {
     var selectedNamespace = $('#namespaceSelect').val();
-    $('.rule-card').hide(); // Hide all rule cards initially
+    $('.rule-card').hide(); 
     $('.rule-card').each(function() {
       var ruleNamespace = $(this).data('namespace').split('/')[0];
       if (ruleNamespace == selectedNamespace) {
-        $(this).show(); // Show only the rule cards with the selected namespace
+        $(this).show(); 
       }
     });
   });
 });
-
-  
